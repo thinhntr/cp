c103c8fd822475c7585b7dd278d33fcf3fbf88bc
Thinh Nguyen <thinhntr@gmail.com>
2022-08-03 18:25:42 +0700

    Optimized using Disjoint Set

--- leetcode/Max Area of Island.py
+++ leetcode/Max Area of Island.py
@@ -34,6 +34,78 @@ class Solution:
                 result = max(result, dfs(i, j))
         return result
 
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        """Iterative DFS"""
+        m, n = len(grid), len(grid[0])
+        visited: Set[Tuple[int, int]] = set()
+        result = 0
+
+        for i in range(m):
+            for j in range(n):
+                stack = [(i, j)]
+                area = 0
+                while stack:
+                    ci, cj = stack.pop()
+                    if (
+                        (ci, cj) in visited
+                        or ci < 0
+                        or ci >= m
+                        or cj < 0
+                        or cj >= n
+                        or not grid[ci][cj]
+                    ):
+                        continue
+                    area += 1
+                    visited.add((ci, cj))
+                    stack.append((ci + 1, cj))
+                    stack.append((ci - 1, cj))
+                    stack.append((ci, cj + 1))
+                    stack.append((ci, cj - 1))
+                result = max(result, area)
+
+        return result
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        """Disjoin Set"""
+        m, n = len(grid), len(grid[0])
+        parent = [i for i in range(m * n)]
+        size = [0] * (m * n)
+
+        def hash_indices(i, j):
+            return i * n + j
+
+        def find_set(u):
+            if u == parent[u]:
+                return u
+            parent[u] = find_set(parent[u])
+            return parent[u]
+
+        def union_sets(u, v):
+            a = find_set(u)
+            b = find_set(v)
+            if a != b:
+                if size[a] < size[b]:
+                    a, b = b, a
+                parent[b] = a
+                size[a] += size[b]
+
+        for i in range(m):
+            for j in range(n):
+                if not grid[i][j]:
+                    continue
+                u = hash_indices(i, j)
+                size[u] = 1
+
+                if i > 0 and grid[i - 1][j]:
+                    v = hash_indices(i - 1, j)
+                    union_sets(u, v)
+
+                if j > 0 and grid[i][j - 1]:
+                    v = hash_indices(i, j - 1)
+                    union_sets(u, v)
+
+        return max(size)
+
 
 t = Tester(Solution())
 
